d23793f3fe141b2c77c94c75f35f584c954172d7
David Calcus <targoo@gmail.com>
2019-09-25 18:01:41 +0100

    cleanup

--- src/modules/hello/status.ts
+++ src/modules/hello/status.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { APIGatewayEvent, Context, Handler, Callback } from 'aws-lambda';
 
-import { successResponse } from '../../utils/lambda-response';
+import { successResponse } from '../../utils';
 
 export const handler: Handler = async (event: APIGatewayEvent, _context: Context, callback: Callback) => {
   const response = successResponse({
---
+++ src/utils/index.ts
@@ -0,0 +1,3 @@
+export { default as runWarm } from './run-warm';
+export * from './lambda-response';
+export { default as dynamo } from './dynamo';
