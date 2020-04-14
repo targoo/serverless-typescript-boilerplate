f0d46bbb75ad935cd83ec64ac8d733f072dbca00
David Calcus <targoo@gmail.com>
2020-04-14 12:21:48 +0100

    cleanup

--- src/modules/hello/status.ts
+++ src/modules/hello/status.ts
@@ -4,8 +4,6 @@ import { APIGatewayEvent, Context, Handler, Callback } from 'aws-lambda';
 import { successResponse } from '../../utils';
 
 export const handler: Handler = async (event: APIGatewayEvent, _context: Context, callback: Callback) => {
-  const test = 1;
-  const t = 1;
   const response = successResponse({
     message: {
       message: 'Go Serverless v2.0! Your function executed successfully!',
